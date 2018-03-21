c8fc3f7aa7b1af85464760d9d0edc5b1c444e167
Iver Myklebust <iverom@gmail.com>
2018-03-21 17:46:44 +0100

    Oppg 1 på i main.c

--- gpio/main.c
+++ gpio/main.c
@@ -1,6 +1,6 @@
-#include <stdint.h>
+include <stdint.h>
 
-#define GPIO ((NRF_GPIO_REGS*)__GPIO_BASE_ADDRESS__)
+#define GPIO ((NRF_GPIO_REGS*)0x50000000)
 
 typedef struct {
 	volatile uint32_t RESERVED0[321];
@@ -11,7 +11,7 @@ typedef struct {
 	volatile uint32_t DIR;
 	volatile uint32_t DIRSET;
 	volatile uint32_t DIRCLR;
-	volatile uint32_t RESERVED1[__RESERVED1_SIZE__];
+	volatile uint32_t RESERVED1[120];
 	volatile uint32_t PIN_CNF[32];
 } NRF_GPIO_REGS;
 
@@ -23,18 +23,30 @@ int main(){
 	}
 
 	// Configure buttons
-	GPIO->PIN_CNF[__BUTTON_A_PIN__] = 0;
-	GPIO->PIN_CNF[__BUTTON_B_PIN__] = 0;
+	GPIO->PIN_CNF[17] = 0;
+	GPIO->PIN_CNF[26] = 0;
 
 	int sleep = 0;
+	//Finner mask_button_a og mask_button_b, hhv 2^17 og 2^26
+	int mask_button_a = 131072
+	int mask_button_b = 67108864
+	
 	while(1){
 
 		/* Check if button B is pressed;
 		 * turn on LED matrix if it is. */
-
+		if ((GPIO->IN & mask_button_a) != mask_button_a){
+			GPIO->OUTCLR = (1 << 13);
+			GPIO->OUTCLR = (1 << 14);
+			GPIO->OUTCLR = (1 << 15); 
+		}
 		/* Check if button A is pressed;
 		 * turn off LED matrix if it is. */
-
+		else if((GPIO->IN & mask_button_b) != mask_button_b){
+			GPIO->OUTSET = (1 << 13); 
+			GPIO->OUTSET = (1 << 14); 
+			GPIO->OUTSET = (1 << 15); 
+		}								
 		sleep = 10000;
 		while(--sleep);
 	}
@@ -42,4 +54,8 @@ int main(){
 }
 
 
-//Knapp A koblet til 
+//Knapp A koblet til pin 17 og knapp B koblet til pin 26
+//De vil være lave ved trykk
+//0x50000000 er base for GPIO 
+//Verdien til PIN_CNF[0] = 0x700 minus verdien til DIRCLR = 0x520 og dele på 4 for å 
+//finne verdien til reserved_size = 120 
